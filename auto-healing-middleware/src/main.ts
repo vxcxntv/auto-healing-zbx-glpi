0ff487716b4305437f92b59712fe1376e775c5cc
vxcxntv <contato.mtvicente@gmail.com>
2026-04-17 16:55:34 -0300

    quarta correction p deploy

--- auto-healing-middleware/src/main.ts
+++ auto-healing-middleware/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = Number(process.env.PORT ?? 3005);
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
+  await app.listen(port);
 }
 void bootstrap();
